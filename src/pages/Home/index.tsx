a95469ff822980a80a027f713149a4559be13560
devleonardorabelo <devleonardorabelo@gmail.com>
2020-09-03 20:45:16 -0300

    chore: added react-native-vector-icons

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
 import RoundedButton from '../../components/Button/Rounded';
+import CircularButton from '../../components/Button/Circular';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { ThemeColors } from '../../contexts/Theme';
 
 const Home: React.FC = () => {
   return (
     <View>
       <RoundedButton title="Confirmar" action={() => console.log('iuuu')} />
       <RoundedButton title="Cancelar" disabled={true} action={() => console.log('iuuu')} />
+      <CircularButton
+        icon={<Icon name="plus" size={16} color={ThemeColors.green} />}
+        action={() => {}}
+      />
     </View>
   );
 };
