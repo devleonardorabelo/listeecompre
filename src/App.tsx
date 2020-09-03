d4bccb7265712b9a947826475b35b6aa9b48cd6d
devleonardorabelo <devleonardorabelo@gmail.com>
2020-09-02 21:30:31 -0300

    chore: added prettier, eslint and typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
