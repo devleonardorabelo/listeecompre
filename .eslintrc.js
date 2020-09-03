a13c293a213b94cdb188108991ac76fe7374dc1d
devleonardorabelo <devleonardorabelo@gmail.com>
2020-09-02 23:09:56 -0300

    chore: added react-navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,34 +11,35 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module"
-    },
-    plugins: [
-      "react",
-      "react-hooks",
-      "@typescript-eslint",
-      "prettier"
-    ],
-    rules: {
-      indent: ['error', 2, { SwitchCase: 1 }],
-      quotes: ['error', 'single', { avoidEscape: true }],
-      'no-empty-function': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      'react/display-name': 'off',
-      'react/prop-types': 'off',
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
       },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
+  },
 };
